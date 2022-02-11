ae4abec43254dae549f918417888070938b0a2c8
mochiyan <98373689+mochiyan@users.noreply.github.com>
2022-02-11 21:38:43 +0900

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+import "./styles.css";
+
+export const App = () => {
+  return (
+    <>
+      <div>
+        <input placeholder="TODOを入力" />
+        <button>追加</button>
+      </div>
+      <div>
+        <p>未完了のTODO</p>
+        <ul>
+          <li></li>
+        </ul>
+      </div>
+      <div></div>
+    </>
+  );
+};
